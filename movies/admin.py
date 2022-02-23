90b3946be18ed5719f1299a05678414c22245278
Candykiller16 <Candykiller16@mail.ru>
2022-02-23 18:10:40 +0300

    second commit

--- movies/admin.py
+++ movies/admin.py
@@ -54,6 +54,7 @@ class MovieAdmin(admin.ModelAdmin):
     list_editable = ['draft', ]  # чтобы редактировать фильм из списка в админке
     form = MovieAdminForm
     readonly_fields = ('get_image',)
+    actions = ['publish', 'unpublish']
     fieldsets = (
         (None, {
             'fields': (('title', 'tagline'),)
@@ -76,8 +77,35 @@ class MovieAdmin(admin.ModelAdmin):
         }),
     )
 
+
+
     def get_image(self, obj):
         return mark_safe(f'<img src={obj.poster.url} width="100" height="100"')
+
+    def unpublish(self, request, queryset):
+        """Снять с публикации"""
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            message_bit = '1 запись была обновлена '
+        else:
+            message_bit = f'{row_update} записей были обновлены'
+        self.message_user(request, f'{message_bit}')
+
+    def publish(self, request, queryset):
+        """Опубликовать"""
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = '1 запись была обновлена '
+        else:
+            message_bit = f'{row_update} записей были обновлены'
+        self.message_user(request, f'{message_bit}')
+
+    publish.short_description = 'Опубликовать'
+    publish.allowed_permissions = ('change',)
+
+    unpublish.short_description = 'Снять с публикации'
+    unpublish.allowed_permissions = ('change',)
+
 
     get_image.short_description = "Постер"
 
